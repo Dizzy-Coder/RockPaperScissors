9b7588381bcc1e0517f05ad57d0da1388145ebb2
redwan <142202213+Dizzy-Coder@users.noreply.github.com>
2024-08-20 23:10:36 +0600

    Add game logic in script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+function getComputerChoice(){
+    let choice = Math.floor(Math.random()*3);
+    if (choice==0) return "Rock";
+    else if (choice==1) return "Paper";
+    else return "Scissors";
+}
+
+function getHumanChoice(){
+    let choice = Number(prompt("Enter Number to choose:\n1. Rock\n2. Paper\n3. Scissors"),"");
+    if (choice==1) return "Rock";
+    else if (choice==2) return "Paper";
+    else if (choice==3) return "Scissors";
+}
+
+let humanScore = 0, computerScore =0;
+
+function playRound(){
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    console.log('You chose: '+humanChoice);
+    console.log("Computer chose: "+computerChoice);
+    if (humanChoice==computerChoice) console.log("It's a draw!");
+    else if (humanChoice=="Rock"){
+        if (computerChoice=="Paper"){
+            console.log("Paper beats Rock!");
+            computerScore++;
+        }
+        else{
+            console.log("Rock beats Scissors!");
+            humanScore++;
+        }
+    }
+    else if (humanChoice=="Paper"){
+        if (computerChoice=="Scissors"){
+            console.log("Scissors beats Paper!");
+            computerScore++;
+        }
+        else{
+            console.log("Paper beats Rock!");
+            humanScore++;
+        }
+    }
+    else if (humanChoice=="Scissors"){
+        if (computerChoice=="Rock"){
+            console.log("Rock beats Scissors!");
+            computerScore++;
+        }
+        else{
+            console.log("Scissors beats Paper!");
+            humanScore++;
+        }
+    }
+    console.log("You: "+humanScore+"    Computer: "+computerScore);
+}
+
+for (let i=0; i<5; i++){
+    playRound();
+}
